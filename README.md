47df1d88edc26cc57e5a06afb2926290ae5cf86b
Mageshyadav M <44959216+mageshyadavm@users.noreply.github.com>
2021-09-08 06:39:17 +0530

    Syncfusion Datagrid using PostSharp sample added

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to populate Syncfusion WPF controls using PostSharp?
+
+This sample loads [Datagrid](https://help.syncfusion.com/wpf/datagrid/getting-started) with business objects incorporating [NotifyPropertyChanged](https://doc.postsharp.net/inotifypropertychanged-add) and [Command](https://doc.postsharp.net/command) attributes from [PostSharp](https://doc.postsharp.net/).
+
+![Syncfusion Datagrid with PostSharp](Images/Output.png)
---
+++ SyncfusionPostSharp/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,60 @@
+﻿using PostSharp.Patterns.Model;
+using PostSharp.Patterns.Xaml;
+using Syncfusion.UI.Xaml.Grid;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SyncfusionPostSharp
+{
+    [NotifyPropertyChanged]
+    public class EmployeeViewModel
+    {
+        private ObservableCollection<Employee> employees;
+
+        public ObservableCollection<Employee> Employees
+        {
+            get { return employees; }
+            set { employees = value; }
+        }
+
+        public string EmployeeEventDetail { get; set; }
+
+        public EmployeeViewModel()
+        {
+            Employees = new ObservableCollection<Employee>();
+            Employees.Add(new Employee("Mark", 10000, 1000));
+            Employees.Add(new Employee("Adam", 20000, 2000));
+            Employees.Add(new Employee("Michael", 30000, 3000));
+            Employees.Add(new Employee("Johnson", 40000, 4000));
+            Employees.Add(new Employee("Gayle", 50000, 5000));
+            Employees.Add(new Employee("Peter", 60000, 6000));
+            Employees.Add(new Employee("William", 70000, 1000));
+            Employees.Add(new Employee("Rob", 80000, 2000));
+            Employees.Add(new Employee("Oliver", 30000, 3000));
+            Employees.Add(new Employee("Jennifer", 40000, 4000));
+            Employees.Add(new Employee("Dave", 50000, 5000));
+            Employees.Add(new Employee("Erica", 60000, 6000)); 
+            Employees.Add(new Employee("William", 20000, 1000));
+            Employees.Add(new Employee("Glenn", 70000, 2000));
+            Employees.Add(new Employee("Helen", 80000, 3000));
+            Employees.Add(new Employee("Randy", 40000, 4000));
+            Employees.Add(new Employee("Kelly", 50000, 5000));
+            Employees.Add(new Employee("Wndy", 60000, 6000));
+        }
+
+        [Command(ExecuteMethod = nameof(ExecuteCellTapped), CanExecuteMethod = nameof(CanExecuteCellTapped))]
+        public ICommand CellTappedCommand { get; set; }
+
+        private bool CanExecuteCellTapped()
+        {
+            return true;
+        }
+
+        private void ExecuteCellTapped(GridCellTappedEventArgs args)
+        {
+           EmployeeEventDetail = (args.Record as Employee).FirstName + " row cell has been tapped";
+        }
+
+    }
+}
